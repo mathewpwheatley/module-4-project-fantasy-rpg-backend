003c73bd95cdd26e47a0083ea85b94dbc0f53ffc
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-07-09 10:46:56 -0700

    updated current round game_save_params

--- app/controllers/game_saves_controller.rb
+++ app/controllers/game_saves_controller.rb
@@ -42,7 +42,7 @@ class GameSavesController < ApplicationController
     private
 
     def game_save_params
-        params.require(:game_save).permit(:user_id, :character_id, :opponent_id, :round)
+        params.require(:game_save).permit(:user_id, :character_id, :opponent_id, :current_round)
     end
 
 end
